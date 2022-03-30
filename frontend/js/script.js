0098fdbbb31d21e81b898976231a5d213a69ef27
Luís Cunha <luismcunha@ua.pt>
2022-03-30 12:03:01 +0100

    troca de README.md

---
+++ frontend/js/script.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const experiencia = document.querySelector('.flex-container');
+
+experiencia.style.backgroundColor = '#dd55ff';
+
+function pokeType() {
+    document.getElementById('drop-type').classList.toggle('show')
+}
