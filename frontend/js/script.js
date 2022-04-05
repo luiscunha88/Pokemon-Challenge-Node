dbe22027ea3a91c5ad1449fea29535c8cae3324b
Luís Cunha <luismcunha@ua.pt>
2022-04-05 10:11:00 +0100

    troca de README.md

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,18 +1,9 @@
-'use strict'
-
-
-const corFlex = document.querySelector('.flex-container').style.backgroundColor='#dd55ff';
+'use-strict'
 
 
 function pokeType() {
-    document.getElementById('type').classList.toggle('show')
+    document.getElementById('poke-type').classList.toggle('hidden')
 }
 
-window.onclick = function(e) {
-    if (!e.target.matches('#pokemon-type')) {
-        let myDropdown = document.getElementById("type");
-        if (myDropdown.classList.contains('show')) {
-            myDropdown.classList.remove('show');
-        }
-    }
-}
+
+
