ebf1c9cf1e2868f2439eafa44fc971d1cddf60a2
Luís Cunha <luismcunha@ua.pt>
2022-04-06 17:57:04 +0100

    troca de README.md ajustes no frontend

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,9 +1,42 @@
 'use-strict'
 
+const pokeType = document.getElementById('poke-type')
+const weaknesses = document.getElementById('weaknesses')
+const type = document.getElementById('type')
 
-function pokeType() {
-    document.getElementById('poke-type').classList.toggle('hidden')
+function dropdownType() {
+    pokeType.classList.toggle('hidden')
 }
 
+function dropdownWeakness() {
+    weaknesses.classList.toggle('hidden')
+}
+
+closeButtons = function (id, element) {
+    window.onclick = function(e) {
+        if (!e.target.matches('id')) {
+            if (!element.classList.contains('hidden')) {
+                element.classList.add('hidden')
+            }
+        }
+    }
+}
+
+closeButtons(pokeType, type)
 
 
+
+
+
+// function pokeType() {
+//     document.getElementById('poke-type').classList.toggle('hidden')
+// }
+//
+// window.onclick = function(e) {
+//     if (!e.target.matches('#type')) {
+//         let myDropdown = document.getElementById("poke-type")
+//         if (!myDropdown.classList.contains('hidden')) {
+//             myDropdown.classList.add('hidden')
+//         }
+//     }
+// }
