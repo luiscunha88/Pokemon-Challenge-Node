547d66fd80c1cd76952e58dc78b96fe40b8557ff
Luís Cunha <luismcunha@ua.pt>
2022-04-18 16:45:18 +0100

    troca de README.md ajustes no frontend calls às api call à api v2 feito com sucesso com o código no ficheiro crawling.js api v1:   - o mesmo código retira um ficheiro html   - mesmo que funcionasse, não há garantias que ficasse bem porque os dados começam por [] e não por {}   - problema resolvido usando o axios para fazer call   - resolver problema do limite de calls à api
     Foram gravados os dados da api v1

--- backend/readFile.js
+++ backend/readFile.js
@@ -10,30 +10,40 @@ const pool = new Pool({
 })
 
 
-
-
-fs.readFile('./api-calls/pokeapi-v2/1.json', "utf-8",  (err, data) => {
+fs.readFile('./api-calls/pokeapi-glitch-v1/1.json', "utf-8",  (err, data) => {
     if (err) {
         console.error(err)
         return
     }
 
     let poke = JSON.parse(data)
-    console.log(poke.id + '\n' + poke.name)
 
-    pool.query(
-        "INSERT INTO pokemon(id, name, weight)VALUES(poke.id, poke.name, poke.weight)",
-        (err, res) => {
-            console.log(err, res);
-            pool.end();
-        }
-    )
+    console.log(poke[0].number + '\n' + poke[0].name)
+
+    pool.query(`UPDATE pokemon SET name = '${poke[0].name}' WHERE id = 1`, (err, res) => {
+        console.log(err, res);
+        pool.end();
+    })
+
 })
 
 
+    // `INSERT INTO pokemon(id, weight, height, base_exp, hp, atk, def, special_attack, special_defense, speed)VALUES('${poke.id}','${poke.weight/10}','${poke.height/10}', '${poke.base_experience}','${poke.stats[0].base_stat}', '${poke.stats[1].base_stat}', '${poke.stats[2].base_stat}', '${poke.stats[3].base_stat}', '${poke.stats[4].base_stat}', '${poke.stats[5].base_stat}' )`,
 
+// pool.query(
+//     `INSERT INTO pokemon(name, species, description, gender, generation)VALUES('${poke[0].name}','${poke[0].species}', '${poke[0].gender}', '${poke[0].description}', '${poke[0].gen}')`,
+//     (err, res) => {
+//         console.log(err, res);
+//         pool.end();
+//     }
+// )
 
 // './api-calls/pokeapi-v2/1.json'
 
 // './api-calls/pokeapi-glitch-v1/1.json'
 //     console.log(pokemon[0].description) - os dados da api v1 são um vetor de objetos
+
+
+// https://jsonworld.com/demo/how-to-connect-nodejs-with-postgresql
+
+// https://kb.objectrocket.com/postgresql/how-to-update-a-postgresql-table-with-node-842#insert+some+records+into+the+postgresql+table
